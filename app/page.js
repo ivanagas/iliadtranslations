63eda559a5c7ed71d73f6d8d9365cf868867b43b
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2023-07-04 21:40:47 -0700

    update homepage

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default async function Home() {
       <h1>Iliad Translations</h1>
       <p>The Iliad of Homer is an epic, ancient Greek poem. It is one of the oldest works of literature still read. It takes place near the end of the siege of Troy by the Greeks (also known as the Trojan War). The story depicts argument between King Agamemnon and the great warrior Achilles, and the ensuing consequences on all parties involved.</p>
       <p>The Iliad has been translated 100s of times. This site lists and compares some of the most popular ones to help you find the version that works for you.</p>
-      <p>See details on individual translations like:</p>
+      <h2>Individual Translations</h2>
       <ul>
         {translators.map((translator) => (
           <li>
@@ -23,7 +23,7 @@ export default async function Home() {
           </li>
         ))}
       </ul>
-      <p>Also see any comparisons between the two like:</p>
+      <h2>Comparisons</h2>
       <ul>
         {comparisonsList.map((comparison) => (
           <li>
@@ -31,27 +31,37 @@ export default async function Home() {
           </li>
         ))}
       </ul>
-      <h4>Future</h4>
-      <p>
-        - Pull popular details and comparisons from PostHog in JSON
-          - Unique users who say they read it (custom event)
-          - Pageviews 
-        - Add tags
-          - Free, oldest, best
-          - Verse, prose
-          - Enable sorting by these
-        - Write tests
-          - Check data.json has all the fields
-          - Check all the pages are generated
-        - More reviews
-        - More translations
-        - More analysis links
-        - Add further readings from each book
-        - How to use this site
-        - Home page compare
-      </p>
-      <Link href="http://johnstoniatexts.x10host.com/homer/homertranslations.html">A site with a full list of Translations.</Link><span> </span>
-      <Link href="http://johnstoniatexts.x10host.com/homer/iliadessay8html.html">Commentary from Ian Johnston on Iliad Translations</Link>
+      <h2>Further reading</h2>
+      <ul>
+        <li><Link href="http://johnstoniatexts.x10host.com/homer/homertranslations.html">A site with a full list of Iliad translations.</Link></li>
+        <li><Link href="http://johnstoniatexts.x10host.com/homer/iliadessay8html.html">Commentary from Ian Johnston on Iliad translations</Link></li>
+      </ul>
+      <h2>Future Todos</h2>
+      <ul>
+        <li>Pull popular details and comparisons from PostHog in JSON</li>
+        <ul>
+          <li>Pageviews</li>
+          <li>Unique users who say they read it (custom event)</li>
+        </ul>
+        <li>Write tests</li>
+        <ul>
+          <li>Check data.json has all the fields</li>
+          <li>Check all the pages are generated</li>
+        </ul>
+        <li>Add tags</li>
+        <ul>
+          <li>Free, oldest, best</li>
+          <li>Verse, prose</li>
+          <li>Enable sorting by these</li>
+        </ul>
+        <li>More reviews</li>
+        <li>More translations</li>
+        <li>More analysis links</li>
+        <li>Add further readings from each book</li>
+        <li>How to use this site</li>
+        <li>Home page compare</li>
+        <li>Quiz to find your translation (popularity, accuracy, prose/verse)</li>
+      </ul>
     </div>
   )
 }
