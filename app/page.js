dcb51b9568174e3896b931ec0c6d15adaf323785
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2023-07-30 14:56:13 -0700

    apos

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default async function Home() {
     <div>
       <h1>Iliad Translations</h1>
       <p>The Iliad of Homer is an epic, ancient Greek poem. It is one of the oldest works of literature still read. It takes place near the end of the siege of Troy by the Greeks (also known as the Trojan War). The story depicts an argument between King Agamemnon and the great warrior Achilles, and the consequences of it on the ongoing war between Trojans and Greeks.</p>
-      <p>The Iliad has been translated 100s of times, so versions vary widely. Some are faithful to Homer's original Greek, others focus on readability. Some are in verse, others in prose. The experience of reading the Iliad can be dramatically different depending on the version you choose.</p>
+      <p>The Iliad has been translated 100s of times, so versions vary widely. Some are faithful to Homer&apos;s original Greek, others focus on readability. Some are in verse, others in prose. The experience of reading the Iliad can be dramatically different depending on the version you choose.</p>
       <p>To help you make this choice, this site lists some of the most popular translations with a summary and passages to see which you like best.</p>
       <h2>Compare Translations</h2>
       <Selector data={translatorData}></Selector>
