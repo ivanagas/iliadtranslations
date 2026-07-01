cd178c60f60b91ba3ce73ce8322ffe3ba3de39bc
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2026-06-30 17:21:42 -0700

    autocapture link clicks

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -5,7 +5,10 @@ posthog.init('phc_9jsDPJWGw09UKF4jBNCthimBdsAMFws7RXddqXaPEVo', {
   ui_host: 'https://us.posthog.com',
   defaults: '2025-11-30',
   disable_session_recording: true,
-  autocapture: false,
+  autocapture: {
+    dom_event_allowlist: ['click'], // only clicks, not change/submit
+    element_allowlist: ['a'],       // only <a> tags (filter to external hrefs in PostHog for outbound clicks)
+  },
   capture_pageleave: false,
   advanced_disable_flags: true,
 });
